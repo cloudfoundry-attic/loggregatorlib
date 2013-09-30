32edce2bbe8d3c2048e0024a7d58e46f3dc2d338
Matthew McNew <mmcnew@pivotallabs.com>
2013-09-30 16:46:45 -0600

    restore old localip only as we need it
    Signed-off-by: Alex Jackson <ajackson@pivotallabs.com>

--- cfcomponent/instrumentation/varz_message.go
+++ cfcomponent/instrumentation/varz_message.go
@@ -40,7 +40,7 @@ func NewVarzMessage(name string, instrumentables []Instrumentable) (*VarzMessage
 	memStats := new(runtime.MemStats)
 	runtime.ReadMemStats(memStats)
 
-	ip, _, err := localip.LocalIPAndPort()
+	ip, err := localip.LocalIP()
 	if err != nil {
 		return &VarzMessage{}, err
 	}
--- cfcomponent/localip/local_ip.go
+++ cfcomponent/localip/local_ip.go
@@ -3,6 +3,27 @@ package localip
 import "net"
 import "strconv"
 
+func LocalIP() (string, error) {
+	addr, err := net.ResolveUDPAddr("udp", "1.2.3.4:1")
+	if err != nil {
+		return "", err
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		return "", err
+	}
+
+	defer conn.Close()
+
+	host, _, err := net.SplitHostPort(conn.LocalAddr().String())
+	if err != nil {
+		return "", err
+	}
+
+	return host, nil
+}
+
 func LocalIPAndPort() (string, uint32, error) {
 	l, err := net.Listen("tcp4", "0.0.0.0:0")
 	if err != nil {
