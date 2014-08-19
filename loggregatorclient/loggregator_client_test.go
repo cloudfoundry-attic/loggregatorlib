523d25d0753e013b870bccaa34025771ad793fd0
Johannes Petzold <jpetzold@pivotal.io>
2014-08-19 10:07:51 -0600

    Ginkgo-ify loggregatorclient tests
    Signed-off-by: John Tuley <jtuley@pivotal.io>

--- loggregatorclient/loggregator_client_test.go
+++ loggregatorclient/loggregator_client_test.go
@@ -1,78 +1,73 @@
-package loggregatorclient
+package loggregatorclient_test
 
 import (
+	"github.com/cloudfoundry/loggregatorlib/loggregatorclient"
 	"github.com/cloudfoundry/gosteno"
-	"github.com/stretchr/testify/assert"
 	"net"
-	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func TestSend(t *testing.T) {
-	expectedOutput := []byte("Important Testmessage")
-	loggregatorClient := NewLoggregatorClient("localhost:9875", gosteno.NewLogger("TestLogger"), 0)
+var _ = Describe("Testing with Ginkgo", func() {
+	It("sends metrics", func() {
+		expectedOutput := []byte("Important Testmessage")
+		loggregatorClient := loggregatorclient.NewLoggregatorClient("localhost:9875", gosteno.NewLogger("TestLogger"), 0)
 
-	udpAddr, err := net.ResolveUDPAddr("udp", "localhost:9875")
-	assert.NoError(t, err)
+		udpAddr, _ := net.ResolveUDPAddr("udp", "localhost:9875")
 
-	udpListener, err := net.ListenUDP("udp", udpAddr)
-	defer udpListener.Close()
-	assert.NoError(t, err)
+		udpListener, _ := net.ListenUDP("udp", udpAddr)
+		defer udpListener.Close()
 
-	loggregatorClient.Send(expectedOutput)
+		loggregatorClient.Send(expectedOutput)
 
-	buffer := make([]byte, 4096)
-	readCount, _, err := udpListener.ReadFromUDP(buffer)
-	assert.NoError(t, err)
+		buffer := make([]byte, 4096)
+		readCount, _, _ := udpListener.ReadFromUDP(buffer)
 
-	received := string(buffer[:readCount])
-	assert.Equal(t, string(expectedOutput), received)
+		received := string(buffer[:readCount])
+		Expect(received).To(Equal(string(expectedOutput)))
 
-	metrics := loggregatorClient.Emit().Metrics
-	assert.Equal(t, len(metrics), 5) //make sure all expected metrics are present
-	for _, metric := range metrics {
-		switch metric.Name {
-		case "currentBufferCount":
-			assert.Equal(t, metric.Value, uint64(0))
-			assert.Equal(t, metric.Tags["loggregatorAddress"], "127.0.0.1")
-		case "sentMessageCount":
-			assert.Equal(t, metric.Value, uint64(1))
-			assert.Equal(t, metric.Tags["loggregatorAddress"], "127.0.0.1")
-		case "sentByteCount":
-			assert.Equal(t, metric.Value, uint64(21))
-			assert.Equal(t, metric.Tags["loggregatorAddress"], "127.0.0.1")
-		case "receivedMessageCount":
-			assert.Equal(t, metric.Value, uint64(1))
-			assert.Equal(t, metric.Tags["loggregatorAddress"], "127.0.0.1")
-		case "receivedByteCount":
-			assert.Equal(t, metric.Value, uint64(21))
-			assert.Equal(t, metric.Tags["loggregatorAddress"], "127.0.0.1")
-		default:
-			t.Error("Got an invalid metric name: ", metric.Name)
+		metrics := loggregatorClient.Emit().Metrics
+			Expect(metrics).To(HaveLen(5))
+
+		for _, metric := range metrics {
+			Expect(metric.Tags).To(HaveKeyWithValue("loggregatorAddress", "127.0.0.1"))
+
+			switch metric.Name {
+			case "currentBufferCount":
+				Expect(metric.Value).To(Equal(uint64(0)))
+			case "sentMessageCount":
+				Expect(metric.Value).To(Equal(uint64(1)))
+			case "sentByteCount":
+				Expect(metric.Value).To(Equal(uint64(21)))
+			case "receivedMessageCount":
+				Expect(metric.Value).To(Equal(uint64(1)))
+			case "receivedByteCount":
+				Expect(metric.Value).To(Equal(uint64(21)))
+			default:
+				Fail("Got an invalid metric name: " + metric.Name)
+			}
 		}
-	}
-}
+	})
 
-func TestDontSendEmptyData(t *testing.T) {
-	bufferSize := 4096
-	firstMessage := []byte("")
-	secondMessage := []byte("hi")
-	loggregatorClient := NewLoggregatorClient("localhost:9875", gosteno.NewLogger("TestLogger"), bufferSize)
+	It("doesn't send empty data", func() {
+		bufferSize := 4096
+		firstMessage := []byte("")
+		secondMessage := []byte("hi")
+		loggregatorClient := loggregatorclient.NewLoggregatorClient("localhost:9875", gosteno.NewLogger("TestLogger"), bufferSize)
 
-	udpAddr, err := net.ResolveUDPAddr("udp", "localhost:9875")
-	assert.NoError(t, err)
+		udpAddr, _ := net.ResolveUDPAddr("udp", "localhost:9875")
 
-	udpListener, err := net.ListenUDP("udp", udpAddr)
-	defer udpListener.Close()
-	assert.NoError(t, err)
+		udpListener, _ := net.ListenUDP("udp", udpAddr)
+		defer udpListener.Close()
 
-	loggregatorClient.Send(firstMessage)
-	loggregatorClient.Send(secondMessage)
+		loggregatorClient.Send(firstMessage)
+		loggregatorClient.Send(secondMessage)
 
-	buffer := make([]byte, bufferSize)
-	readCount, _, err := udpListener.ReadFromUDP(buffer)
+		buffer := make([]byte, bufferSize)
+		readCount, _, _ := udpListener.ReadFromUDP(buffer)
 
-	assert.NoError(t, err)
-
-	received := string(buffer[:readCount])
-	assert.Equal(t, string(secondMessage), received)
-}
+		received := string(buffer[:readCount])
+		Expect(received).To(Equal(string(secondMessage)))
+	})
+})
