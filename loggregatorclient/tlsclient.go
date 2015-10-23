27eb55f04824914e4a3f60fe6b971f2bdbeb0fec
Michael Fraenkel <fraenkel@us.ibm.com>
2015-10-23 10:54:51 -0400

    Implement TLSClient (TCP for now)
    [#104193518]

--- loggregatorclient/tlsclient.go
+++ loggregatorclient/tlsclient.go
@@ -1,18 +1,31 @@
 package loggregatorclient
 
-import "github.com/cloudfoundry/gosteno"
+import (
+	"net"
+	"sync"
+	"time"
+
+	"github.com/cloudfoundry/gosteno"
+)
+
+const timeout = 1 * time.Second
 
 type tlsClient struct {
 	address string
 	logger  *gosteno.Logger
+
+	lock sync.Mutex
+	conn net.Conn
 }
 
 func NewTLSClient(logger *gosteno.Logger, address string) (Client, error) {
-	loggregatorClient := &tlsClient{
+	c := &tlsClient{
 		address: address,
 		logger:  logger,
 	}
-	return loggregatorClient, nil
+
+	_ = c.connect()
+	return c, nil
 }
 
 func (c *tlsClient) Scheme() string {
@@ -24,8 +37,38 @@ func (c *tlsClient) Address() string {
 }
 
 func (c *tlsClient) Stop() {
+	c.lock.Lock()
+	if c.conn != nil {
+		c.conn.Close()
+		c.conn = nil
+	}
+	c.lock.Unlock()
 }
 
 func (c *tlsClient) Send(data []byte) {
-	c.logger.Warn("Sending over TLS (unsupported)")
+	c.lock.Lock()
+	if c.conn == nil {
+		if c.connect() != nil {
+			return
+		}
+	}
+	conn := c.conn
+	c.lock.Unlock()
+
+	_, err := conn.Write(data)
+	if err != nil {
+		c.Stop()
+	}
+}
+
+func (c *tlsClient) connect() error {
+	var err error
+	c.conn, err = net.DialTimeout("tcp", c.address, timeout)
+	if err != nil {
+		c.logger.Warnd(map[string]interface{}{
+			"error":   err,
+			"address": c.address,
+		}, "Failed to connect over TLS")
+	}
+	return err
 }
---
+++ loggregatorclient/tlsclient_test.go
@@ -0,0 +1,143 @@
+package loggregatorclient_test
+
+import (
+	"net"
+	"time"
+
+	"github.com/cloudfoundry/gosteno"
+	"github.com/cloudfoundry/loggregatorlib/loggregatorclient"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("TLS Client", func() {
+	var (
+		logger      *gosteno.Logger
+		tlsListener net.Listener
+		connChan    chan net.Conn
+
+		client loggregatorclient.Client
+	)
+
+	BeforeEach(func() {
+		logger = gosteno.NewLogger("test")
+
+		var err error
+		tlsListener, err = net.Listen("tcp", ":0")
+		Expect(err).NotTo(HaveOccurred())
+
+		connChan = make(chan net.Conn, 1)
+
+		tlsListener := tlsListener
+		connChan := connChan
+		go func() {
+			defer GinkgoRecover()
+			for {
+				conn, err := tlsListener.Accept()
+				if err != nil {
+					return
+				}
+				if conn != nil {
+					connChan <- conn
+				}
+			}
+		}()
+	})
+
+	JustBeforeEach(func() {
+		var err error
+		client, err = loggregatorclient.NewTLSClient(logger, tlsListener.Addr().String())
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		tlsListener.Close()
+		client.Stop()
+	})
+
+	Describe("NewTLSClient", func() {
+		It("attempts to connect", func() {
+			Expect(client).NotTo(BeNil())
+			Eventually(connChan).Should(Receive())
+		})
+
+		Context("when the connect fails", func() {
+			BeforeEach(func() {
+				tlsListener.Close()
+			})
+
+			It("returns a client", func() {
+				Expect(client).NotTo(BeNil())
+				Consistently(connChan).ShouldNot(Receive())
+			})
+		})
+	})
+
+	Describe("Scheme", func() {
+		It("returns tls", func() {
+			Expect(client.Scheme()).To(Equal("tls"))
+		})
+	})
+
+	Describe("Address", func() {
+		It("returns the address", func() {
+			Expect(client.Address()).To(Equal(tlsListener.Addr().String()))
+		})
+	})
+
+	Describe("Sending data", func() {
+		var conn net.Conn
+
+		JustBeforeEach(func() {
+			Eventually(connChan).Should(Receive(&conn))
+			Expect(conn).NotTo(BeNil())
+		})
+
+		It("sends data", func() {
+			client.Send([]byte("abc"))
+			bytes := make([]byte, 10)
+			n, err := conn.Read(bytes)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(bytes[:n]).To(Equal([]byte("abc")))
+		})
+
+		Context("when there is no data", func() {
+			It("does not send", func() {
+				client.Send([]byte(""))
+				bytes := make([]byte, 10)
+				conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
+				_, err := conn.Read(bytes)
+				Expect(err).To(HaveOccurred())
+				opErr := err.(*net.OpError)
+				Expect(opErr.Timeout()).To(BeTrue())
+			})
+		})
+
+		Context("when the connection is closed", func() {
+			It("reconnects and sends", func() {
+				client.Stop()
+				client.Send([]byte("abc"))
+
+				Eventually(connChan).Should(Receive(&conn))
+				Expect(conn).NotTo(BeNil())
+				bytes := make([]byte, 10)
+				n, err := conn.Read(bytes)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(bytes[:n]).To(Equal([]byte("abc")))
+			})
+		})
+	})
+
+	Describe("Stop", func() {
+		It("can be called multiple times", func() {
+			done := make(chan struct{})
+			go func() {
+				client.Stop()
+				client.Stop()
+				close(done)
+			}()
+			Eventually(done).Should(BeClosed())
+		})
+	})
+})
