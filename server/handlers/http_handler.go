9e579b1ddbee87b576713bc01556a4d07053a1b3
Nino Khodabandeh <ninok@hpe.com>
2016-09-08 10:39:46 -0600

    Export the HttpHandler
    It was being returned from an exported function.  It should have been exported.
    
    Signed-off-by: Sam Nelson <snelson@pivotal.io>

--- server/handlers/http_handler.go
+++ server/handlers/http_handler.go
@@ -7,16 +7,16 @@ import (
 	"github.com/cloudfoundry/gosteno"
 )
 
-type httpHandler struct {
-	messages <-chan []byte
+type HttpHandler struct {
+	Messages <-chan []byte
 	logger   *gosteno.Logger
 }
 
-func NewHttpHandler(m <-chan []byte, logger *gosteno.Logger) *httpHandler {
-	return &httpHandler{messages: m, logger: logger}
+func NewHttpHandler(m <-chan []byte, logger *gosteno.Logger) *HttpHandler {
+	return &HttpHandler{Messages: m, logger: logger}
 }
 
-func (h *httpHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+func (h *HttpHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	h.logger.Debugf("http handler: ServeHTTP entered with request %v", r.URL)
 	defer h.logger.Debugf("http handler: ServeHTTP exited")
 
@@ -25,7 +25,7 @@ func (h *httpHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 	rw.Header().Set("Content-Type", `multipart/x-protobuf; boundary=`+mp.Boundary())
 
-	for message := range h.messages {
+	for message := range h.Messages {
 		partWriter, err := mp.CreatePart(nil)
 		if err != nil {
 			h.logger.Infof("http handler: Client went away while serving recent logs")
